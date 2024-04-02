44fc0d11131b1c8683ff42bad13e024bdad8228d
Nahid-07 <nahidasif1998@gmail.com>
2024-04-02 22:36:35 +0600

    added footter

--- app/component/product/ProductCard.jsx
+++ app/component/product/ProductCard.jsx
@@ -6,10 +6,12 @@ const ProductCard =async () => {
     const products = await getData()
     console.log(products);
     return (
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5 mt-12">
+        <div className="bg-gray-200 p-4 my-12 rounded-lg">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5">
             {
                 products?.map(product => <Card key={product.id} product={product} />)
             }
+        </div>
         </div>
     );
 };
--- app/footer/Footer.js
+++ app/footer/Footer.js
@@ -1,9 +1,20 @@
+import { FaFacebook, FaInstagram, FaWhatsapp } from "react-icons/fa";
+
 
 
 const Footer = () => {
     return (
-        <div className="c">
-            
+        <div className="bg-gray-900 py-12 px-4 text-gray-200">
+            <div className="max-w-7xl mx-auto">
+                <h1 className="text-2xl font-semibold ">Artians' Mart</h1>
+                <div className="flex gap-5 text-3xl my-5">
+                    <FaFacebook/>
+                    <FaInstagram />
+                    <FaWhatsapp />
+                </div>
+                <h3 className="text-sm">Narinda, Sharatgupta road, Dhaka-100</h3>
+            </div>
+
         </div>
     );
 };
